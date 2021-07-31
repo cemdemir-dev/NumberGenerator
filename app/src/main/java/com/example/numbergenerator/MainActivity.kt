085b4d57a2f88966352d6af516b51c9950b3884d
cemdemir-dev <cemdemircem@gmail.com>
2021-08-01 00:17:49 +0300

    adds View Binding techs.

--- app/src/main/java/com/example/numbergenerator/MainActivity.kt
+++ app/src/main/java/com/example/numbergenerator/MainActivity.kt
@@ -3,18 +3,24 @@ package com.example.numbergenerator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.numbergenerator.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    fun generateNumber(view: View){
-        val myRandomInt = Random.nextInt(until = 10000)
-        numberText.text = "$myRandomInt"
+        generateNumber()
+    }
+    fun generateNumber(){
+        binding.generateButton.setOnClickListener(){
+            val myRandomInt = Random.nextInt(until = 10000)
+            binding.numberText.text = "$myRandomInt"
+        }
     }
 }
